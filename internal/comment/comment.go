648d7d885ee27d2c414165c08a8b270d9134e66e
rbrtrss <roberto.rousse@gmail.com>
2022-08-12 02:07:30 -0300

    added comment service layer

---
+++ internal/comment/comment.go
@@ -0,0 +1,40 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+// Comment - the representation of our app comment service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - struc where all of our logi will be built upon
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new (empu for now) service
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("Retreiving a comment")
+	cmt, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+	return cmt, nil
+}
